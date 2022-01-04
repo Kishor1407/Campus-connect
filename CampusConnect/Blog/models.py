ff814230c4886c4605c890abe104984d9d704460
Kartik <nandagaonkartik@gmail.com>
2022-01-04 22:47:10 +0530

    done it

--- CampusConnect/Blog/models.py
+++ CampusConnect/Blog/models.py
@@ -1,15 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
 from ckeditor_uploader.fields import RichTextUploadingField
+
 # Create your models here.
 class Blogpost(models.Model):
     post_id = models.AutoField(primary_key=True)
     user = models.ForeignKey(User,on_delete=models.CASCADE)
+    content=RichTextUploadingField()
     title = models.CharField(max_length=50)
-    content = RichTextUploadingField()
-    pub_date = models.DateField()
+    pub_date = models.DateField(null=True)
     Banner_image = models.ImageField(upload_to='blog/images', default="")
 
     def __str__(self):
         return self.title
+
 
